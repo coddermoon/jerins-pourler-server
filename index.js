9335b10e18744a0bf11a7868343fb0c6af0378cb
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-04 17:41:06 +0600

    server basic setup done

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-
+const port = process.env.PORT ||5000
 const app = express();
 app.use(express.json())
 app.use(cors());
+
 require('dotenv').config()
+
+app.get('/',(req,res)=>{
+    res.send('server is running at http://localhost:5000')
+})
+
+app.listen(port,()=>console.log('listening to port '+port))
